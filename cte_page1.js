2e12095e3d2a26be0ce3c8f7adc34527ce08830c
matthewc07 <143449262+matthewc07@users.noreply.github.com>
2024-01-30 12:58:40 -0800

    Create cte_page1.js

---
+++ cte_page1.js
@@ -0,0 +1,7 @@
+let SFTWR_DEV_PAGE = document.querySelector("#compsci");
+
+
+SFTWR_DEV_PAGE.addEventListener("click", GoToSoftwarePage);
+function GoToSoftwarePage(){
+    window.location.href = "secondpage.html";
+}
